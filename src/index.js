c7db792f5b7f41ba413133615289bd777c6ab52f
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-10-27 16:10:02 -0700

    Add IE11 polyfills

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+// IE11 polyfills must be first
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
 
